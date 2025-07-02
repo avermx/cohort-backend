4b20073dee9d0ef6fe0244b7a5c17ac7d33cecaa
avermx <aniket125verma@gmail.com>
2025-07-02 20:32:06 +0530

    init

---
+++ backend-02/index.js
@@ -0,0 +1,24 @@
+// const http = require('http')
+
+// const server = http.createServer((req, res)=>{
+//     res.end('auhoooooooo')
+// })
+
+// server.listen(3000,()=>{
+//     console.log('server gand mein daal le');
+    
+// })
+const express = require('express')
+
+
+const app = express()
+
+
+app.get('/home',(req,res)=>{
+    res.send('helloo jI')
+    
+})
+app.listen(3000,()=>{
+    console.log('hekkk');
+    
+})
